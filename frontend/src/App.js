611a902e4cca845628cc307d33bd68855f4125fd
Christian Sobkowski <christian@othello.lan>
2023-01-22 14:01:18 +0100

    Added auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,20 @@ import ThemeProvider from './theme';
 // components
 import ScrollToTop from './components/scroll-to-top';
 import { StyledChart } from './components/chart';
+import { Authenticator } from "@aws-amplify/ui-react";
+import RequireAuth from './RequireAuth';
+
 
 // ----------------------------------------------------------------------
 
 export default function App() {
   return (
-    <ThemeProvider>
+  <Authenticator.Provider>
+   <ThemeProvider>
       <ScrollToTop />
       <StyledChart />
       <Router />
     </ThemeProvider>
+  </Authenticator.Provider>
   );
 }
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -10,6 +10,11 @@ import Iconify from '../components/iconify';
 // sections
 import { LoginForm } from '../sections/auth/login';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator, View } from '@aws-amplify/ui-react';
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router';
+
 // ----------------------------------------------------------------------
 
 const StyledRoot = styled('div')(({ theme }) => ({
@@ -42,66 +47,52 @@ const StyledContent = styled('div')(({ theme }) => ({
 
 export default function LoginPage() {
   const mdUp = useResponsive('up', 'md');
+  const { route } = useAuthenticator((context) => [context.route]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  let from = location.state?.from?.pathname || '/';
+
+  const components = {
+    SignUp: {
+      Footer() {
+        return (
+          <View textAlign="center">
+            <strong>Password Policy</strong>:
+            <ul>
+              <li>Minimum of 8 characters</li>
+              <li>At least one lowercase character</li>
+              <li>At least one uppercase character</li>
+              <li>At least one number character</li>
+              <li>At least one symbol character</li>
+            </ul>
+          </View>
+        );
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (route === 'authenticated') {
+      navigate(from, { replace: true });
+    }
+  }, [route, navigate, from]);
 
   return (
-    <>
-      <Helmet>
-        <title> Login | Minimal UI </title>
-      </Helmet>
-
-      <StyledRoot>
-        <Logo
-          sx={{
-            position: 'fixed',
-            top: { xs: 16, sm: 24, md: 40 },
-            left: { xs: 16, sm: 24, md: 40 },
-          }}
-        />
-
-        {mdUp && (
-          <StyledSection>
+  <>
+    <Helmet>
+      <title> Login | Minimal UI </title>
+    </Helmet>
+    <StyledSection>
             <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
               Hi, Welcome Back
             </Typography>
             <img src="/assets/illustrations/illustration_login.png" alt="login" />
           </StyledSection>
-        )}
-
-        <Container maxWidth="sm">
-          <StyledContent>
-            <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
-            </Typography>
-
-            <Typography variant="body2" sx={{ mb: 5 }}>
-              Don’t have an account? {''}
-              <Link variant="subtitle2">Get started</Link>
-            </Typography>
-
-            <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-              </Button>
-            </Stack>
-
-            <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                OR
-              </Typography>
-            </Divider>
-
-            <LoginForm />
-          </StyledContent>
-        </Container>
-      </StyledRoot>
-    </>
+    <View className="auth-wrapper">
+      <Authenticator components={components}/>
+    </View>
+  </>
+    
   );
+ 
 }
